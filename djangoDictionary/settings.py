ed690b2cdf5892f1635cb8df74a1e1f9481046ae
popsoft01 <popoolatunde52@gmail.com>
2021-08-29 21:15:01 +0100

    This is my dictionary project

--- djangoDictionary/settings.py
+++ djangoDictionary/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dictionary'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'djangoDictionary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR +'/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
             ],
         },
     },
