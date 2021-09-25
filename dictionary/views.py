63f23809d1449dd1a4687c777b9a8f8c85c5122c
popsoft01 <popoolatunde52@gmail.com>
2021-09-25 18:31:10 +0100

    This is my dictionary project

--- dictionary/views.py
+++ dictionary/views.py
@@ -13,8 +13,8 @@ def word(request):
     meaning = dictionary.meaning(search)
     synonyms = dictionary.synonym(search)
     antonyms = dictionary.antonym(search)
-    context= {
-        'meaning': meaning,
+    context = {
+        'meaning': meaning['Noun'][0],
         'synonyms': synonyms,
         'antonyms': antonyms
     }
